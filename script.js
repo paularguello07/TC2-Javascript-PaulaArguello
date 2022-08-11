c5b499a592fe51c63035f3c8135758356a9eaf2f
Paula Andrea Arguello Gutierrez <68118520+paularguello07@users.noreply.github.com>
2022-08-11 16:22:17 -0500

    final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function agregar(){
     const input = document.querySelector("#entrada");
     const texto = document.createElement("li"); 
@@ -22,3 +23,63 @@ function eliminar_todo(){
     }
 
 }
+function tiempo(){
+    
+    contar();
+    cont = setInterval(contar,20)
+    document.querySelector("#start").disabled = true;
+}
+function parar(){
+    
+    clearInterval(cont)
+    document.querySelector("#start").disabled = false;
+}
+
+function reiniciar(){
+    mili=0,segundo=0, minutos=0, horas=0;
+    clearInterval(cont)
+    imprimir(mili,segundo,minutos,horas);
+    document.querySelector("#start").disabled = false;
+}
+let mili=0,segundo=0, minutos=0, horas=0;
+function contar(){
+    mili++;
+    if (mili>59){
+        segundo++;
+        mili=0;
+    }
+    if (segundo>59){
+        minutos++;
+        segundo=0;
+    }
+    if (minutos>59){
+        horas++;
+        minutos=0;
+    }
+    if (horas>24){
+        horas=0;
+    }
+
+    imprimir(mili,segundo,minutos,horas)
+}
+
+function imprimir(mil,seg,min,hor){
+    if(mil<10){
+        mil = "0" + mil;
+    }
+    if(seg<10){
+        seg = "0" + seg;
+    }
+    if(min<10){
+        min = "0" + min;
+    }
+    if(hor<10){
+        hor = "0" + hor;
+    }
+
+    document.querySelector("#horas").innerHTML = hor;
+    document.querySelector("#minutos").innerHTML = min;
+    document.querySelector("#segundos").innerHTML = seg;
+    document.querySelector("#milisegundos").innerHTML = mil;
+}
+
